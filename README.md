cf370402431860026d5d4321875485279d7dc7d4
Ian Mckay <contact@ianmckay.com.au>
2022-09-14 22:33:28 +1000

    Add tree view script

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# tree-view-cfn
-Force CloudFormation to generate a tree view for any stack
+# TreeViewCFN
+
+Force CloudFormation to generate a tree view for any stack.
+
+![](https://raw.githubusercontent.com/iann0036/iann0036/master/static/treeview.png)
+
+> **Warning**
+> This script will convert your templates to JSON. You should ensure you have a source template backup if using YAML.
+
+## Usage
+
+To convert a local file and output the result to console:
+
+```
+python3 convert.py --filename mystack.yaml
+```
+
+Or, to convert a deployed CloudFormation stack:
+
+```
+python3 convert.py --stack MyStack
+```
+
+### Options
+
+```
+usage: convert.py [-h] (--filename FILENAME | --stack STACK) [--region REGION] [--profile PROFILE]
+
+Converts templates or stacks to be tree-view compliant
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --filename FILENAME  The filename of a local template you wish to convert, will output to console
+  --stack STACK        The stack name of a remote stack you wish to convert, will update in place
+  --region REGION      (optional) The region name to use when polling remote stacks
+  --profile PROFILE    (optional) The profile name to use when polling remote stacks
+```
---
+++ convert.py
@@ -0,0 +1,152 @@
+import boto3
+import sys
+import json
+import re
+from cfn_flip import to_json
+import argparse
+
+
+def resolvePaths(template, prop, loop_check, paths):
+    if isinstance(prop, dict):
+        if 'Ref' in prop:
+            if prop['Ref'] in template['Resources'].keys():
+                paths += [prop['Ref']]
+        elif 'Fn::GetAtt' in prop:
+            if prop['Fn::GetAtt'][0] in template['Resources'].keys():
+                paths += [prop['GetAtt'][0]]
+        elif 'Fn::Sub' in prop:
+            substr = prop['Fn::Sub']
+            exclusions = []
+            if isinstance(prop['Fn::Sub'], list) and not isinstance(prop['Fn::Sub'], str):
+                substr = prop['Fn::Sub'][0]
+                exclusions = prop['Fn::Sub'][1].keys()
+            r1 = re.findall(r"\$\{(\w+)", substr)
+            paths = [x for x in r1 if x not in exclusions]
+        for v in prop.values():
+            paths = resolvePaths(template, v, loop_check, paths)
+    elif isinstance(prop, list) and not isinstance(prop, str):
+        for listitem in prop:
+            paths = resolvePaths(template, listitem, loop_check, paths)
+    
+    return paths
+
+
+def getFullPaths(res_paths, target, current_paths):
+    new_paths = []
+    for pathitem in res_paths[target]:
+        extended_paths = []
+        for current_path in current_paths:
+            extended_paths.append([pathitem] + current_path)
+        new_paths += getFullPaths(res_paths, pathitem, extended_paths)
+    
+    if len(new_paths) == 0:
+        return current_paths
+
+    return new_paths
+
+
+def generateTemplate(original_template):
+    template = json.loads(to_json(original_template))
+
+    res_paths = {}
+    for k in template['Resources'].keys():
+        prop = {}
+        if template['Resources'][k].get('Properties') is not None:
+            prop = template['Resources'][k]['Properties']
+        
+        res_paths[k] = resolvePaths(template, prop, [k], [])
+
+    for k in template['Resources'].keys():
+        fullpaths = getFullPaths(res_paths, k, [[k]])
+
+        max_length = 999
+        selected_path = []
+        for fullpath in fullpaths:
+            if len(fullpath) < max_length:
+                max_length = len(fullpath)
+                selected_path = fullpath
+
+        metadata = dict()
+        if template['Resources'][k].get('Metadata') is not None:
+            metadata.update(template['Resources'][k]['Metadata'])
+        metadata.update({
+            "aws:cdk:path": "TreeViewCFN/{}/Resource".format('/'.join(selected_path))
+        })
+        template['Resources'][k]['Metadata'] = metadata
+
+    return json.dumps(template)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Converts templates or stacks to be tree-view compliant')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--filename", help="The filename of a local template you wish to convert, will output to console")
+    group.add_argument("--stack", help="The stack name of a remote stack you wish to convert, will update in place")
+    parser.add_argument("--region", help="(optional) The region name to use when polling remote stacks")
+    parser.add_argument("--profile", help="(optional) The profile name to use when polling remote stacks")
+    args = parser.parse_args()
+    
+    if args.filename is not None:
+        file_content = ''
+        with open(sys.argv[1]) as f:
+            file_content = f.read()
+
+        print(generateTemplate(file_content))
+    elif args.stack is not None:
+        session = boto3.session.Session(profile_name=args.profile)
+        cfnclient = session.client('cloudformation', region_name=args.region)
+
+        stacks = []
+        try:
+            stacks = cfnclient.describe_stacks(
+                StackName=args.stack
+            )['Stacks']
+        except:
+            sys.exit('could not find the requested stack')
+
+        if len(stacks) != 1:
+            sys.exit('too many returned stacks')
+
+        stack_params = []
+        if 'Parameters' in stacks[0]:
+            stack_params = stacks[0]['Parameters']
+
+        original_template = ''
+        try:
+            original_template = cfnclient.get_template(
+                StackName=stacks[0]['StackId'],
+                TemplateStage='Processed'
+            )['TemplateBody']
+        except:
+            sys.exit('could not retrieve template body')
+
+        if original_template == '':
+            sys.exit('could not retrieve template body')
+
+        if not isinstance(original_template, str):
+            original_template = json.dumps(original_template)
+
+        updated_template = generateTemplate(original_template)
+
+        cfnclient.update_stack(
+            StackName=stacks[0]['StackId'],
+            TemplateBody=updated_template,
+            Capabilities=[
+                'CAPABILITY_NAMED_IAM',
+                'CAPABILITY_AUTO_EXPAND'
+            ],
+            Parameters=stack_params
+        )
+
+        waiter = cfnclient.get_waiter('stack_update_complete')
+        waiter.wait(
+            StackName=stacks[0]['StackId'],
+            WaiterConfig={
+                'Delay': 2,
+                'MaxAttempts': 10
+            }
+        )
+
+        print("successfully updated stack")
+    else:
+        raise Exception("unknown options")
